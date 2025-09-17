d603950524548f4f013e05c3dddb0ffc4db96502
HadiAbu <hadi.abuhamed@gmail.com>
2025-09-17 16:58:32 +0700

    added some tests

--- src/test/java/com/example/BasicTest.java
+++ src/test/java/com/example/BasicTest.java
@@ -4,12 +4,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.time.Duration;
 
 public class BasicTest {
-    WebDriver driver;
+    WebDriver driver;    
+    // WebDriverWait wait;
 
     @BeforeClass
     public void setup() {
@@ -17,14 +20,35 @@ public class BasicTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        // wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
     }
 
-    @Test
+    @Test(enabled=false)   
     public void openGoogleAndSearch() {
-        driver.get("https://www.google.com");
+        driver.get("https://www.google.com/?hl=en");
         driver.findElement(By.name("q")).sendKeys("Selenium WebDriver");
         driver.findElement(By.name("q")).submit();
     }
+    //test classname
+    @Test
+    public void openWikiAndSearch() {
+        driver.get("https://en.wikipedia.org/wiki/Taco");
+        String text= driver.findElement(By.className("infobox-caption")).getText();
+         Assert.assertTrue(text.contains("Three varieties of taco "),
+                "Expected text 'Three varieties of taco ' was not found on the page!");
+
+    }
+    //test xpath
+    @Test
+    public void openWebsiteAndSearch() {
+        driver.get("https://en.wikipedia.org/wiki/Taco");
+        String text= driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div[1]/table/tbody/tr[1]/td/div/a[3]")).getText();
+         Assert.assertTrue(text.contains("al pastor"),
+                "Expected text 'al pastor' was not found on the page!");
+
+    }
+
 
     @AfterClass
     public void teardown() {
